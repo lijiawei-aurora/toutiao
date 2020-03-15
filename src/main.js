77d0c9c29d595c86998e868e2e5d86152dbee2a3
iljiawei <1030855421@qq.com>
2020-03-15 11:08:54 +0800

    注册使用移动端组件vant

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入vant移动端组件库 完全导入 后期优化可使用哪个引入哪个
+import Vant from 'vant'
+import 'vant/lib/index.css' // 引入vant组件样式
+Vue.use(Vant) // 全局注册 相当于调用了vant的install方法
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -12,11 +12,27 @@ const routes = [
   {
     path: '/home',
     component: Home
-
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/article',
+    component: () => import('@/views/article')
+  },
+  {
+    path: '/article',
+    component: () => import('@/views/article')
+  },
+  {
+    path: '/article',
+    component: () => import('@/views/article')
+  },
+  {
+    path: '/article',
+    component: () => import('@/views/article')
+  },
+  {
+    path: '/article',
+    component: () => import('@/views/article')
   }
 ]
 
