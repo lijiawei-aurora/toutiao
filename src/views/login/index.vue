b66b3ba31e80f47c8b2126488ce2c6ed102e0b56
iljiawei <1030855421@qq.com>
2020-03-16 16:45:43 +0800

    登录界面

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,23 +1,30 @@
 <template>
-
- <div class="container">
-  <!-- 导航  go(-1) 回到上一个历史记录 -->
-  <van-nav-bar title="登录" left-arrow @click-left="$router.go(-1)"></van-nav-bar>
- <!-- 该容器组件提供外边框 -->
-  <van-cell-group>
-    <!-- 手机号 -->
-    <van-filed label="手机号"></van-filed>
-
-  </van-cell-group>
-</div>
+  <div class="container">
+    <!-- 导航  go(-1) 回到上一个历史记录 -->
+    <van-nav-bar title="登录" left-arrow @click-left="$router.go(-1)"></van-nav-bar>
+    <!-- 该容器组件提供外边框 -->
+    <van-cell-group>
+      <!-- 手机号 -->
+      <van-field label="手机号" placeholder="请输入手机号"></van-field>
+      <van-field label="验证码" placeholder="请输入验证码">
+        <!-- 组件的扩展-slot 插槽来自定义内容
+        slot的值有4个，label，left-icon，icon，button-->
+        <van-button slot="button" round size="small" type="info">发送验证码</van-button>
+      </van-field>
+    </van-cell-group>
+    <!-- round为圆角  block属性可以将按钮的元素类型设置为块级元素-->
+    <div class="login-box">
+      <van-button type="info" size="small" block round>登录</van-button>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style>
-
+.login-box {
+  padding: 20px;
+}
 </style>
