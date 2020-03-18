0088f9098213f497ed607882ac21539aeb83875c
iljiawei <1030855421@qq.com>
2020-03-18 16:17:19 +0800

    使用tabs组件简单布局文章列表页

---
+++ src/views/home/components/article-list.vue
@@ -0,0 +1,38 @@
+<template>
+  <!-- 文章列表组件 -->
+  <!-- 上拉加载 -->
+  <!-- 滚动条 -->
+  <div class="scrollwrapper">
+      <!-- List 组件通过loading和finished两个变量控制加载状态，当组件滚动到底部时，会触发load事件并将loading设置成true。此时可以发起异步操作并更新数据，数据更新完毕后，将loading设置成false即可。若数据已全部加载完毕，则直接将finished设置成true即可。 -->
+      <!-- 文章列表  loading表示是否在上拉加载
+      finished表示数据是否已经都加载完毕-->
+      <!-- van-list 初始化完毕后会自动检测距离底部的距离 -->
+  <van-list v-model="loading" @load="onload" ></van-list>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loading: false, // 是否开启了上拉加载，默认值为false
+      finished: false // 是否已经完成所有数据的加载
+    }
+  },
+  methods: {
+    // 一秒后停止加载
+    onload () {
+      setTimeout(() => {
+        this.finished = true
+      }, 1000)
+    }
+
+  }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,22 +1,91 @@
 <template>
-  <div><div class="split"></div>
-  <van-skeleton title avatar :row="3"></van-skeleton>
-<div class="split"></div>
-  <van-skeleton title avatar :row="3"></van-skeleton>
-<div class="split"></div>
-  <van-skeleton title avatar :row="3"></van-skeleton>
-<div class="split"></div>
-  <van-skeleton title avatar :row="3"></van-skeleton>
+  <div class="home">
+    <!-- 放置tabs组件 -->
+    <van-tabs>
+      <!-- 内部放置若干个标签 -->
+      <van-tab v-for="item in 10" :key="item" :title="`标签${item}`">
+        <!-- 设置滚动条 -->
+        <!-- <div class="scroll-wrapper">
+          <van-cell-group>
+            单元格
+            <van-cell title="标题" value="内容"></van-cell>
+          </van-cell-group>
+        </div> -->
+        <ArticleList></ArticleList>
+      </van-tab>
+      <span class="bar_btn">
+      <van-icon name="wap-nav"></van-icon>
+    </span>
+    </van-tabs>
 
   </div>
 </template>
 
 <script>
+import ArticleList from './components/article-list'
 export default {
-
+  name: 'Home',
+  components: {
+    ArticleList
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.van-tabs {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  /deep/ .van-tabs__wrap {
+    height: 36px;
+    padding-right: 36px;
+    .van-tab {
+      line-height: 36px;
+    }
+    .van-tabs__line {
+      background-color: #3296fa;
+      height: 2px;
+    }
+  }
+  /deep/ .van-tabs__content {
+    flex: 1;
+    overflow: hidden;
+  }
+  /deep/ .van-tab__pane {
+    height: 100%;
+    .scroll-wrapper {
+      height: 100%;
+      overflow-y: auto;
+    }
+  }
+}
+ // 需要放置编辑频道图标的位置
+.bar_btn {
+  width: 36px;
+  height: 35px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  &::before {
+    content: '';
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: 999;
+    box-shadow: 0 0 10px #999;
+    transform: scale(1, 0.6);
+  }
+  .van-icon-wap-nav {
+    width: 100%;
+    height: 100%;
+    background: #fff;
+    text-align: center;
+    line-height: 35px;
+    position: relative;
+    z-index: 1000;
+    &::before {
+      font-size: 20px;
+    }
+  }
+}
 </style>
