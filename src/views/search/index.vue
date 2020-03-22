4a60fefc61fe5f631f0997ac5303e8428d5c1caf
iljiawei <1030855421@qq.com>
2020-03-22 21:27:16 +0800

    搜索中心联想和历史记录的互斥显示

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -1,16 +1,72 @@
 <template>
-<div class="container">
-  <!-- 导航  go(-1) 回到上一个历史记录 -->
-  <van-nav-bar title="搜索中心" left-arrow @click-left="$router.go(-1)"></van-nav-bar>
-</div>
+  <div class="container">
+    <!-- 搜索组件一级路由   返回上一个页面-->
+    <van-nav-bar left-arrow title="搜索中心" @click-left="$router.back()"></van-nav-bar>
+    <!-- 导航 -->
+    <van-search  placeholder="请输入搜索关键词" v-model="q" shape="round" />
+    <!-- 联想功能  联想和历史记录功能只显示一个 -->
+    <van-cell-group class="suggest-box" v-if="q"  >
+      <van-cell icon="search">
+        <span>j</span>ava
+      </van-cell>
+    </van-cell-group>
+    <!-- 历史记录功能 -->
+    <div class="history-box" v-else>
+      <div class="head">
+        <span>历史记录</span>
+        <van-icon name="delete"></van-icon>
+       </div>
+      <van-cell-group>
+        <van-cell>
+          <a class="word_btn">电脑</a>
+          <van-icon class="close_btn" slot="right-icon" name="cross" />
+        </van-cell>
+      </van-cell-group>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  name: 'search',
+  data () {
+    return { q: '' }
+  }
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.history-box {
+  padding: 0 20px;
+  .head{
+    line-height: 36px;
+    color: #999;
+    .van-icon{
+      font-size: 16px;
+      float: right;
+      margin-top: 10px;;
+    }
+  }
+  .van-cell{
+    padding: 10px 0;
+  }
+  .word_btn{
+    color:#3296fa;
+  }
+  .close_btn{
+    margin-top:5px;
+    color: #999;
+  }
+}
+.suggest-box{
+  /deep/ .van-cell{
+    padding: 10px 20px;
+    color: #999;
+    p{
+      span{
+        color: red;
+      }
+    }
+  }
+}
 </style>
